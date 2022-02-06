8acead678dc1ae131efda02a2ec133490bdefa43
Indimeco <18373729+Indimeco@users.noreply.github.com>
2022-02-06 14:05:46 +1100

    load decks from json

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,59 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-  ( someFunc,
+  ( Card (Card),
+    CardModifierType,
+    cardName,
+    cardFlavorText,
+    cardModifiers,
+    cardConditions,
+    Deck (Deck),
+    deckCards,
+    deckName,
+    deckId,
+    loadDeck,
   )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn "duck"
+import Data.Aeson
+import qualified Data.ByteString.Lazy as BS
+import GHC.Generics
+
+data CardModifierType = Murder | Valor deriving (Enum)
+
+data CardModifierValue = CardModifierType Int deriving (Show, Generic)
+
+instance Data.Aeson.ToJSON CardModifierValue where
+  toEncoding = Data.Aeson.genericToEncoding Data.Aeson.defaultOptions
+
+instance Data.Aeson.FromJSON CardModifierValue
+
+data Card = Card
+  { cardName :: String,
+    cardFlavorText :: String,
+    cardModifiers :: [CardModifierValue],
+    cardConditions :: [CardModifierValue]
+  }
+  deriving (Generic, Show)
+
+instance Data.Aeson.ToJSON Card
+
+instance Data.Aeson.FromJSON Card
+
+data Deck = Deck
+  { deckName :: String,
+    deckId :: Int,
+    deckCards :: [Card]
+  }
+  deriving (Generic, Show)
+
+instance Data.Aeson.ToJSON Deck
+
+instance Data.Aeson.FromJSON Deck
+
+getJSON :: FilePath -> IO BS.ByteString
+getJSON filePath = BS.readFile filePath
+
+loadDeck :: FilePath -> IO (Either String Deck)
+loadDeck filePath = (eitherDecode <$> getJSON filePath) :: IO (Either String Deck)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,26 @@
 import Control.Exception (evaluate)
+import Data.Aeson
+import Data.ByteString.Lazy
+import qualified Data.ByteString.Lazy.Char8 as C
+import Lib
 import Test.Hspec
 import Test.QuickCheck
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.read" $ do
-    it "can parse integers" $ do
-      read "10" `shouldBe` (10 :: Int)
+  describe "Card loading" $ do
+    it "can read a card without modifiers" $ do
+      let rawJson = C.pack "{\"cardName\": \"Lord of Valor\", \"cardFlavorText\": \"The Knight King on his silver throne\", \"cardModifiers\": [], \"cardConditions\": []}"
+      let parsedJson = Data.Aeson.decode rawJson :: Maybe (Card)
+      fmap Lib.cardName parsedJson `shouldBe` Just "Lord of Valor"
 
-    it "can parse floating-point numbers" $ do
-      read "2.5" `shouldBe` (2.5 :: Float)
+    it "can import a deck of cards from a file" $ do
+      content <- Data.ByteString.Lazy.readFile "./test/testCards1.json"
+      let cards = Data.Aeson.decode content :: Maybe [Card]
+      fmap Prelude.length cards `shouldBe` Just 3
+      fmap (Prelude.map Lib.cardName) cards `shouldBe` Just ["Mason of Death", "Knight of Night", "Ring of Avarice"]
+
+    it "can import a deck of cards from a file 2" $ do
+      eitherDeck <- Lib.loadDeck "./test/testDeck1.json"
+      Prelude.length <$> Lib.deckCards <$> eitherDeck `shouldBe` Right 3
+      (Prelude.map Lib.cardName) <$> Lib.deckCards <$> eitherDeck `shouldBe` Right ["Mason of Death", "Knight of Night", "Ring of Avarice"]
